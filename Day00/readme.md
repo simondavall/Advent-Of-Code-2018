4ce3594f154ea519e0f9f6fbbe7e550a94bf1573
Simon Da Vall <simon.davall@gmail.com>
2026-01-21 18:07:48 +0000

    Removed default Day00 from git, other tidying

--- Day00/readme.md
+++
@@ -1,3 +0,0 @@
-### --- Day 0: Advent Of Code --- ###
-https://adventofcode.com/2018/day/0
-
--- Day21/main.js
+++ Day21/main.js
@@ -15,39 +15,39 @@ let r5 = 0;
   while (r1 < input.length){
 
     switch (r1){
-      case 0: r4 = 123; break;
-      case 1: r4 = r4 & 456; break;
-      case 2: r4 = boolToInt(r4 == 72); break;
-      case 3: r1 = r4 + r1; break;
-      case 4: r1 = 0; break;
-      case 5: r4 = 0; break;
-      case 6: r3 = r4 | 65536; break;
-      case 7: r4 = 12670166; break;
-      case 8: r2 = r3 & 255; break;
-      case 9: r4 = r4 + r2; break;
-      case 10: r4 = r4 & 16777215; break;
-      case 11: r4 = r4 * 65899; break;
-      case 12: r4 = r4 & 16777215; break;
-      case 13: r2 = boolToInt(256 > r3); break;
-      case 14: r1 = r2 + r1; break;
-      case 15: r1 = r1 + 1; break;
-      case 16: r1 = 27; break;
-      case 17: r2 = 0; break;
-      case 18: r5 = r2 + 1; break;
-      case 19: r5 = r5 * 256; break;
-      case 20: r5 = boolToInt(r5 > r3); break;
-      case 21: r1 = r5 + r1; break;
-      case 22: r1 = r1 + 1; break;
-      case 23: r1 = 25; break;
-      case 24: r2 = r2 + 1; break;
-      case 25: r1 = 17; break;
-      case 26: r3 = r2; break;
-      case 27: r1 = 7; break;
+      case 0: r4 = 123;                     break;
+      case 1: r4 = r4 & 456;                break;
+      case 2: r4 = boolToInt(r4 == 72);     break;
+      case 3: r1 = r4 + r1;                 break;
+      case 4: r1 = 0;                       break;
+      case 5: r4 = 0;                       break;
+      case 6: r3 = r4 | 65536;              break;
+      case 7: r4 = 12670166;                break;
+      case 8: r2 = r3 & 255;                break;
+      case 9: r4 = r4 + r2;                 break;
+      case 10: r4 = r4 & 16777215;          break;
+      case 11: r4 = r4 * 65899;             break;
+      case 12: r4 = r4 & 16777215;          break;
+      case 13: r2 = boolToInt(256 > r3);    break;
+      case 14: r1 = r2 + r1;                break;
+      case 15: r1 = r1 + 1;                 break;
+      case 16: r1 = 27;                     break;
+      case 17: r2 = 0;                      break;
+      case 18: r5 = r2 + 1;                 break;
+      case 19: r5 = r5 * 256;               break;
+      case 20: r5 = boolToInt(r5 > r3);     break;
+      case 21: r1 = r5 + r1;                break;
+      case 22: r1 = r1 + 1;                 break;
+      case 23: r1 = 25;                     break;
+      case 24: r2 = r2 + 1;                 break;
+      case 25: r1 = 17;                     break;
+      case 26: r3 = r2;                     break;
+      case 27: r1 = 7;                      break;
       case 28: 
         return r4;
-        r2 = boolToInt(r4 == r0); break;
-      case 29: r1 = r2 + r1; break;
-      case 30: r1 = 5; break;
+        r2 = boolToInt(r4 == r0);           break;
+      case 29: r1 = r2 + r1;                break;
+      case 30: r1 = 5;                      break;
     }
     r1 += 1;
   }
@@ -76,34 +76,34 @@ let foundRepeat = false;
   while (!foundRepeat){
 
     switch (r1){
-      case 0: r4 = 123; break;
-      case 1: r4 = r4 & 456; break;
-      case 2: r4 = boolToInt(r4 == 72); break;
-      case 3: r1 = r4 + r1; break;
-      case 4: r1 = 0; break;
-      case 5: r4 = 0; break;
-      case 6: r3 = r4 | 65536; break;
-      case 7: r4 = 12670166; break;
-      case 8: r2 = r3 & 255; break;
-      case 9: r4 = r4 + r2; break;
-      case 10: r4 = r4 & 16777215; break;
-      case 11: r4 = r4 * 65899; break;
-      case 12: r4 = r4 & 16777215; break;
-      case 13: r2 = boolToInt(256 > r3); break;
-      case 14: r1 = r2 + r1; break;
-      case 15: r1 = r1 + 1; break;
-      case 16: r1 = 27; break;
-      case 17: r2 = 0; break;
-      case 18: r5 = r2 + 1; break;
-      case 19: r5 = r5 * 256; break;
-      case 20: r5 = boolToInt(r5 > r3); break;
-      case 21: r1 = r5 + r1; break;
-      case 22: r1 = r1 + 1; break;
-      case 23: r1 = 25; break;
-      case 24: r2 = r2 + 1; break;
-      case 25: r1 = 17; break;
-      case 26: r3 = r2; break;
-      case 27: r1 = 7; break;
+      case 0: r4 = 123;                     break;
+      case 1: r4 = r4 & 456;                break;
+      case 2: r4 = boolToInt(r4 == 72);     break;
+      case 3: r1 = r4 + r1;                 break;
+      case 4: r1 = 0;                       break;
+      case 5: r4 = 0;                       break;
+      case 6: r3 = r4 | 65536;              break;
+      case 7: r4 = 12670166;                break;
+      case 8: r2 = r3 & 255;                break;
+      case 9: r4 = r4 + r2;                 break;
+      case 10: r4 = r4 & 16777215;          break;
+      case 11: r4 = r4 * 65899;             break;
+      case 12: r4 = r4 & 16777215;          break;
+      case 13: r2 = boolToInt(256 > r3);    break;
+      case 14: r1 = r2 + r1;                break;
+      case 15: r1 = r1 + 1;                 break;
+      case 16: r1 = 27;                     break;
+      case 17: r2 = 0;                      break;
+      case 18: r5 = r2 + 1;                 break;
+      case 19: r5 = r5 * 256;               break;
+      case 20: r5 = boolToInt(r5 > r3);     break;
+      case 21: r1 = r5 + r1;                break;
+      case 22: r1 = r1 + 1;                 break;
+      case 23: r1 = 25;                     break;
+      case 24: r2 = r2 + 1;                 break;
+      case 25: r1 = 17;                     break;
+      case 26: r3 = r2;                     break;
+      case 27: r1 = 7;                      break;
       case 28:
         if (seen[r4]){
           foundRepeat = true;
@@ -111,9 +111,9 @@ let foundRepeat = false;
         }
         seen[r4] = true;
         lastValue = r4;
-        r2 = boolToInt(r4 == r0); break;
-      case 29: r1 = r2 + r1; break;
-      case 30: r1 = 5; break;
+        r2 = boolToInt(r4 == r0);           break;
+      case 29: r1 = r2 + r1;                break;
+      case 30: r1 = 5;                      break;
     }
     r1 += 1;
   }
